148926b8dde4c5f145a0f14707ff797ce09428c0
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-04-05 15:44:41 +0100

    go

--- application/views/paiement/recu.php
+++ application/views/paiement/recu.php
@@ -24,43 +24,41 @@
                 </div>  
               <div class="card-body" id="contrat" name="contrat">
                
-                <div class="container-fluid d-flex">
-                  <div class="row">
-                    <div class="col-lg-2 ">
-                        <img src="<?php echo base_url() ?>assets/images/logo-inverse.png" width="75%">
-                      </div>
-                    <div class="col-lg-10 ">
-                      <b>Queen Online</b>
-                      <p>MC 35,Route mornag,Boujardga , Ben Arous 2090.</p>
-                      <p><b>e-mail :</b> info@queenpark.tn</p>
-                      <p><b>mobile :</b> 54 419 959 </p>
-                      <p><b>Fixe :</b> 79 153 352 </p>
-                        
-                    </div>
-
-                  </div>
-                  
-                </div>
+              
+                
+                  <table>
+                       <tr><td width="20%"> <img src="<?php echo base_url() ?>assets/images/logo-inverse.png" width="75%"></td>
+                      <td>
+                        <table>
+                          <tr><td>Queen Park </td></tr>
+                          <tr><td><p>MC 35,Route mornag,Boujardga , Ben Arous 2090.</p> </td></tr>
+                          <tr><td> <p><b>e-mail :</b> info@queenpark.tn</p> </td></tr>
+                          <tr><td> <p><b>mobile :</b> 54 419 959 </p> </td></tr>
+                          <tr> <td><p><b>Fixe :</b> 79 153 352 </p> </td></tr>
+                        </table> 
+                        </td>
+                    </tr>  
+                  </table>
                 
                 <hr>
-                <br><br><br><br><br><br><br><br><br>
+                <br><br><br>
                                   
-                <div class="container-fluid d-flex justify-content-between">   
-                    <div class="row">
-                      
-                    <p  class="col-lg-12 mb-2 ">
+              
+                  
+                      <h3>Recu de paiement</h3><br>
+                    <p >
                       <b>Sujet :</b> &nbsp; Location de l'espace <?php echo $projectInfo->salle ?> pour  le <?php echo $projectInfo->dateDebut ?>  <?php echo $projectInfo->heureDebut ?> au  <?php echo $projectInfo->heureFin ?> 
                     </p>
                      
                     <br>
-                    <p class="col-lg-12 mb-2 ">
+                   
                       <b>Récu par:</b> &nbsp; <?php echo $projectInfo->clientName ; ?> 
-                    </p>  
+                      <br><br><br><br><br>
+                 
                       
-                      <br><br><br><br>
-                    </div > 
+             
 
-                  </div>
+                
                   <table class="table" width="100%">
                         <thead>
                           <tr>
@@ -77,7 +75,7 @@
                             <td><?php echo $res->paiementId  ?></td>
                             <td><?php $date = new DateTime($res->createdDate); echo $date->format('d/m/Y H:i');  ?></td>
                              <td><?php echo  $res->libele   ;  ?></td>
-                              <td><?php  echo  $res->valeur   ;  ?> DT</td>
+                              <td class="text-right"><?php  echo  $res->valeur   ;  ?> DT</td>
                             
                           </tr>
                           <?php } ?>
@@ -107,12 +105,12 @@
 
 
 
-                           <br><br><br><br> <br><br><br><br><br><br><br><br>
+                           <br><br><br><br> 
                            <div style="text-align: right" >
                              <b >à Mornag , Le <?php echo  Date('d/m/Y') ?> <br> Signature </b>
                            </div>
                         
-                </div>
+             
 
 
   
