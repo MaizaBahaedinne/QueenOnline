fae294b1b779666909a49011bd81831d3546fd00
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-04-06 11:40:21 +0100

    Update Reservation.php

--- application/controllers/Reservation.php
+++ application/controllers/Reservation.php
@@ -294,7 +294,7 @@ class Reservation extends BaseController
             $data['userID'] = $this->vendorId ; 
 
 
-             $data['voiture'] = $this->voiture_model->ReservationInfo($data['projectInfo']->reservationVId);
+             $data['voiture'] = $this->voiture_model->ReservationInfo($data['projectInfo']->voiture);
 
             $this->global['pageTitle'] = 'CodeInsect : User Listing';
             $this->loadViews("reservation/details", $this->global, $data, NULL);
