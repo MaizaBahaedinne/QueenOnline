9dd6da1bdf492ed63ff07ad1fd0df5ba3103d8ac
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2022-04-08 09:59:44 +0100

    Update Reservation_model.php

--- application/models/Reservation_model.php
+++ application/models/Reservation_model.php
@@ -94,8 +94,7 @@ class Reservation_model extends CI_Model
     {
         $this->db->select('');
         $this->db->from('statmounth as BaseTbl');
-        $this->db->where('BaseTbl.statut IN (0) ');
-        
+ 
         $query = $this->db->get();
         $result = $query->result();        
         return $result;
@@ -114,7 +113,7 @@ class Reservation_model extends CI_Model
     {
         $this->db->select('sum(COUNT) as COUNT , BaseTbl.YEAR');
         $this->db->from('statmounth as BaseTbl');
-        $this->db->where('BaseTbl.statut IN (0) ');
+
         $this->db->group_by('YEAR');
         
         $query = $this->db->get();
