d9748d92bb9e69d4651483e6aaba9548f10eafc3
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-08-16 17:00:08 +0100

    Update new.php

--- application/views/voiture/new.php
+++ application/views/voiture/new.php
@@ -55,7 +55,7 @@
                                                             <option value=""></option>
                                                             <option value="Limou'queen">Limou'Queen (chrysler 300c)</option>
                                                             <option value="Queen traction">Queen Traction (citroen traction)</option>
-                                                            <option value="Queen traction">Queen Lioness (peugeot 203)</option>
+                                                            <option value="Queen Lioness">Queen Lioness (peugeot 203)</option>
                                                         </select>
                                                     </div>
                                                 </div>
