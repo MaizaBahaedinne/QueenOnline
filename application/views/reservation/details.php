dd2e9b66247479eb29bf3c2c891186f4b12fb7b3
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-08-16 21:16:20 +0100

    Update details.php

--- application/views/reservation/details.php
+++ application/views/reservation/details.php
@@ -50,7 +50,7 @@
                <h5>Troupe</h5>
              </div>
              <div class="card-body">
-                  <?php if ($projectInfo->troupe == 0 ){ echo '<a style="color: white"  class="btn btn-info btn-block" href='.base_url().'Prestation/addNew/'.$projectInfo->reservationId.' >Ajouter</a> ';}  ?>
+                  <?php if ($projectInfo->troupe == 0 ){ echo '<a style="color: white"  class="btn btn-info btn-block" href='.base_url().'Troupe/addNew/'.$projectInfo->reservationId.' >Ajouter</a> ';}  ?>
 
                   <?php  echo '<a style="color: white"  class="btn btn-info btn-block" href='.base_url().'Prestation/addNew/'.$projectInfo->reservationId.' >Ajouter une prestation</a> '; ?>
              </div>
