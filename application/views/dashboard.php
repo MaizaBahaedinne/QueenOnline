12d8fe58c667c193bd48764e1697ca1d26354445
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-08-17 18:11:34 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -16,8 +16,8 @@
    </div>
    <div class="tabs-animation">
       <div class="row">
-         <div class="col-md-6 col-xl-4">
-            <div class="card mb-3 widget-content">
+         <div class="col-md-6 col-xl-4 ">
+            <div class="card mb-3 widget-content bg-dark">
                <div class="widget-content-outer">
                   <div class="widget-content-wrapper">
                      <div class="widget-content-left">
@@ -43,7 +43,7 @@
             </div>
          </div>
          <div class="col-md-6 col-xl-4">
-            <div class="card mb-3 widget-content">
+            <div class="card mb-3 widget-content bg-dark">
                <div class="widget-content-outer">
                   <div class="widget-content-wrapper">
                      <div class="widget-content-left">
@@ -69,7 +69,7 @@
             </div>
          </div>
          <div class="col-md-6 col-xl-4">
-            <div class="card mb-3 widget-content">
+            <div class="card mb-3 widget-content bg-dark">
                <div class="widget-content-outer">
                   <div class="widget-content-wrapper">
                      <div class="widget-content-left">
@@ -115,7 +115,7 @@
             </div>
          </div>
          <div class="col-lg-8 col-xl-8">
-            <div class="mb-3 card">
+            <div class="mb-3 card bg-dark">
                <div class="card-header-tab card-header">
                   <div class="card-header-title">
                      <i class="header-icon lnr-rocket icon-gradient bg-tempting-azure"> </i>
@@ -258,7 +258,7 @@
             </div>
          </div>
          <div class="col-lg-4">
-            <div class="mb-3 card">
+            <div class="mb-3 card bg-dark">
                <div class="card-header-tab card-header">
                   <div class="card-header-title">
                      <i class="header-icon lnr-rocket icon-gradient bg-tempting-azure"> </i>
@@ -302,7 +302,7 @@
                    chart.render();
                
             </script>
-            <div class="mb-3 card">
+            <div class="mb-3 card bg-dark">
                <div class="card-header-tab card-header">
                   <div class="card-header-title">
                      <i class="header-icon lnr-rocket icon-gradient bg-tempting-azure"> </i>
