faab54b88aaf95d3c17483186832a8dbce96495b
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-08-17 18:18:27 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -111,7 +111,7 @@
                      
                   </div>
                </div>
-               <div class="divider mb-0"></div>
+              
             </div>
          </div>
          <div class="col-lg-8 col-xl-8">
