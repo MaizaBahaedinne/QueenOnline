0faf1b249152ba2a79c2e3249c5aa021f0497e55
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-08-21 15:58:07 +0100

    Update details.php

--- application/views/reservation/details.php
+++ application/views/reservation/details.php
@@ -164,7 +164,7 @@
 
 
                         <div class="modal fade bd-example-modal-lg" id="changements" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                          <div class="modal-dialog" role="document">
+                          <div class="modal-dialog modal-lg" role="document">
                             <div class="modal-content">
                               <div class="modal-header">
                                 <h5 class="modal-title" id="exampleModalLabel">Annulation de la reservation</h5>
