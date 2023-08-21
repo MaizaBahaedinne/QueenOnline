c8e246af2b592c1edae45fc508e2dba70f32d49c
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-08-21 15:22:57 +0100

    Update details.php

--- application/views/reservation/details.php
+++ application/views/reservation/details.php
@@ -35,6 +35,29 @@
                         <i class="nav-link-icon lnr-file-empty"></i>
                         <span> Annuler</span>
                         </a>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                          Launch demo modal
+                        </button>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Annulation de la reservation</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                              </div>
+                              <div class="modal-body">
+                                 modal 1
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
                      </li>
                   </ul>
                </div>
@@ -137,9 +160,8 @@
                <?php  echo $projectInfo->noteAdmin ;  ?>
              </div>
              <hr>
-             <?php  foreach ($Backups as $backup ) {   ?>
+             <?php  count($Backups) ;   ?>
                   
-             <?php  }  ?>
          </div>
           
            
