72e1b230376c57d0e4fd9a3859c8e00722ab32ae
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-09-07 15:10:12 +0100

    Update Voiture.php

--- application/controllers/Voiture.php
+++ application/controllers/Voiture.php
@@ -288,10 +288,22 @@ class Voiture extends BaseController
                                  $this->voiture_model->editReservation($reservationInfoe, $resId); 
                             }
 
-                           
+                    
+                                        $koussayMobile = "55465244";
+                                        $mySms = $this->name . " a reservé ".$voitureName." pour le ".$date ;
+                                        $this->sendSMS("216" . $koussayMobile, $mySms);
+                                        
+
+                                        $HaythemMobile = "54419959";
+                                        $mySms = $this->name . " a reservé ".$voitureName." pour le ".$date ;
+                                        $this->sendSMS("216" . $HaythemMobile, $mySms);
+
+
+                                     
 
 
                     $this->session->set_flashdata('success', 'Reservation mise à jour avec succées ');
+
 
 
                     redirect('Voiture/view/'.$result);
@@ -397,6 +409,44 @@ class Voiture extends BaseController
 
 
 
+
+        function http_response($url)
+        {
+                $ch = curl_init();
+                $options = [
+                        CURLOPT_URL => $url,
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_HEADER => false,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_ENCODING => "",
+                        CURLOPT_AUTOREFERER => true,
+                        CURLOPT_CONNECTTIMEOUT => 120,
+                        CURLOPT_TIMEOUT => 120,
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_SSL_VERIFYPEER => false,
+                ];
+                curl_setopt_array($ch, $options);
+                $response = curl_exec($ch);
+                $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                if ($httpCode != 200) {
+                        return "Return code is {$httpCode} \n" . curl_error($ch);
+                } else {
+                        //echo "<pre>".htmlspecialchars($response)."</pre>";
+                        return $response;
+                }
+                curl_close($ch);
+        }
+        function sendSMS($myMobile, $mySms)
+        {
+                $mySender = "Queen park";
+                $key = "ns1PwxEKAljejzi3RSBAHPsoQl/P9s0jtrXDkRb4j6sjNpzNER8aprZNyzyAuLlteKM222LwbgBRrlBCvFDV4YlQbSvBZMYA/Ye3r0ggsYQ=";
+                $Url_str = "www.tunisiesms.tn/client/Api/Api.aspx?fct=sms&key=%KEY%&mobile=%MSISDN%&sms=%SMS%&sender=%SENDER%";
+                $Url_str = str_replace("%MSISDN%", $myMobile, $Url_str);
+                $Url_str = str_replace("%SMS%", urlencode($mySms), $Url_str);
+                $Url_str = str_replace("%SENDER%", urlencode($mySender), $Url_str);
+                $Url_str = str_replace("%KEY%", urlencode($key), $Url_str);
+                echo $this->http_response($Url_str);
+        }
 
 
 }
