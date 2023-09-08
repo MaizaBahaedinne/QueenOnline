7a4bb5db01db860689ebce851d4053c45a2d337c
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-09-08 12:17:12 +0100

    Create Finance.php

---
+++ application/controllers/Finance.php
@@ -0,0 +1,231 @@
+ <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+require APPPATH . '/libraries/BaseController.php';
+
+/**
+ * Class : User (UserController)
+ * User Class to control all user related operations.
+ * @author : Kishor Mali
+ * @version : 1.1
+ * @since : 15 November 2016
+ */
+
+class Finance extends BaseController
+{
+
+
+
+
+
+
+    /**
+     * This is default constructor of the class
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('user_model');
+        $this->load->model('client_model');
+        $this->load->model('reservation_model');
+        $this->load->model('salle_model');
+        $this->load->model('paiement_model');
+        $this->load->model('finance_model');
+        
+        $this->isLoggedIn();   
+    }
+    
+    /**
+     * This function used to load the first screen of the user
+     */
+    public function index()
+    {
+            $data['financeRecords'] = $this->finance_model->paiemenentListing();
+            
+
+            $this->global['pageTitle'] = 'User Listing';
+            $this->loadViews("finance/view", $this->global, $data, NULL);
+    }
+
+
+        /**
+     * This function used to load the first screen of the user
+     */
+    public function voiture()
+    {
+            $data['financeRecords'] = $this->finance_model->paiemenentVoitureListing();
+            
+
+            $this->global['pageTitle'] = 'Finance voiture';
+            $this->loadViews("finance/view", $this->global, $data, NULL);
+    }
+
+
+
+            /**
+     * This function used to load the first screen of the user
+     */
+    public function prestation()
+    {
+            $data['financeRecords'] = $this->finance_model->paiemenentPrestationListing();
+            
+
+            $this->global['pageTitle'] = 'Finance prestation';
+            $this->loadViews("finance/view", $this->global, $data, NULL);
+    }
+
+
+                /**
+     * This function used to load the first screen of the user
+     */
+    public function troupe()
+    {
+            $data['financeRecords'] = $this->finance_model->paiemenentTroupeListing();
+            
+
+            $this->global['pageTitle'] = 'Finance troupe';
+            $this->loadViews("finance/view", $this->global, $data, NULL);
+    }
+
+
+                    /**
+     * This function used to load the first screen of the user
+     */
+    public function photographe()
+    {
+            $data['financeRecords'] = $this->finance_model->paiemenentPhotographeListing();
+            
+
+            $this->global['pageTitle'] = 'Finance troupe';
+            $this->loadViews("finance/view", $this->global, $data, NULL);
+    }
+
+
+
+    /**
+     * This function used to load the first screen of the user
+     */
+    public function relance()
+    {
+            $data['financeRecords'] = $this->finance_model->ReservationCalender();
+            foreach ($data['financeRecords'] as $f ) {
+                $f->relance =   sizeof( $this->finance_model->relanceListing($f->reservationId));
+                $f->last = $this->finance_model->lastrelanceListing($f->reservationId) ;
+
+            }
+
+            $this->global['pageTitle'] = 'User Listing';
+            $this->loadViews("finance/relance", $this->global, $data, NULL);
+    }
+
+
+
+  /**
+     * This function used to load the first screen of the user
+     */
+    public function addRelance()
+    {
+            
+
+            $check = $this->input->post('check'); 
+
+            foreach ($check as $c ) {
+               
+                    $data['projectInfo'] = $this->reservation_model->ReservationInfo($c);
+                    $data['clientInfo'] = $this->user_model->getUserInfo($data['projectInfo']->clientId);
+                    $data['paiementInfo'] = $this->paiement_model->paiementListingbyReservation($c) ;
+                    
+                        $val =  0 ;
+                        foreach ($data['paiementInfo'] as $p ) {
+                                $val = $val + $p->valeur ;                            
+                        }
+
+                     $mySms = "Bonjour ".$data['clientInfo']->prenom.", Nous venons de vous rappeler que le reste de votre réservation ".($data['projectInfo']->prix - $val) ." DT a expiré. Pour éviter l'annulation du contrat, veuillez payer ces dus."  ;
+                    
+                         $myMobile = $data['clientInfo']->mobile ;
+                           $this->sendSMS("216".$myMobile, $mySms) ;
+
+
+                     $relanceInfo = array(
+                        'createdDTM'=>date('Y-m-d H:i:s'),
+                        'createdBy'=>$this->vendorId,
+                        'reservationId'=>$c,                           
+                       );
+
+                      $this->finance_model->addNewRelance($relanceInfo) ;
+
+            }
+
+
+            redirect("Finance/relance") ;
+    }
+
+             
+
+
+
+    function http_response($url)
+        {
+            $ch = curl_init();
+
+            $options = array(
+                CURLOPT_URL            => $url ,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HEADER         => false,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_ENCODING       => "",
+                CURLOPT_AUTOREFERER    => true,
+                CURLOPT_CONNECTTIMEOUT => 120,
+                CURLOPT_TIMEOUT        => 120,
+                CURLOPT_MAXREDIRS      => 10,
+                CURLOPT_SSL_VERIFYPEER => false,
+            );
+            curl_setopt_array( $ch, $options );
+            $response = curl_exec($ch);
+           
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+            if ( $httpCode != 200 ){
+                return "Return code is {$httpCode} \n"
+                    .curl_error($ch);
+            } else {
+                //echo "<pre>".htmlspecialchars($response)."</pre>";
+                return $response;
+            }
+
+            curl_close($ch);
+        }
+
+
+
+        function sendSMS($myMobile, $mySms)
+        {
+
+        $mySender = 'Queen park';
+        $key = "ns1PwxEKAljejzi3RSBAHPsoQl/P9s0jtrXDkRb4j6sjNpzNER8aprZNyzyAuLlteKM222LwbgBRrlBCvFDV4YlQbSvBZMYA/Ye3r0ggsYQ=";
+
+        $Url_str ="www.tunisiesms.tn/client/Api/Api.aspx?fct=sms&key=%KEY%&mobile=%MSISDN%&sms=%SMS%&sender=%SENDER%";
+
+        $Url_str = str_replace("%MSISDN%",$myMobile,$Url_str);
+        $Url_str = str_replace("%SMS%",urlencode($mySms),$Url_str);
+        $Url_str = str_replace("%SENDER%",urlencode($mySender),$Url_str);
+        $Url_str = str_replace("%KEY%",urlencode($key),$Url_str);
+
+        
+        echo $this->http_response($Url_str);
+
+        }
+
+
+
+        
+
+
+
+
+
+
+   
+ 
+}
+
+?>
