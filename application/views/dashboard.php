5d493af87516195b1dab1c52a21150ecf4e0e47d
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-09-21 22:14:51 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -17,7 +17,7 @@
    <div class="tabs-animation">
       <div class="row">
          <div class="col-md-6 col-xl-4">
-            <div class="card mb-3 widget-content">
+            <div class="card mb-3 widget-content widget-content bg-premium-dark">
                <div class="widget-content-outer">
                   <div class="widget-content-wrapper">
                      <div class="widget-content-left">
@@ -43,7 +43,7 @@
             </div>
          </div>
          <div class="col-md-6 col-xl-4">
-            <div class="card mb-3 widget-content">
+            <div class="card mb-3 widget-content widget-content bg-premium-dark">
                <div class="widget-content-outer">
                   <div class="widget-content-wrapper">
                      <div class="widget-content-left">
@@ -69,7 +69,7 @@
             </div>
          </div>
          <div class="col-md-6 col-xl-4">
-            <div class="card mb-3 widget-content">
+            <div class="card mb-3 widget-content widget-content bg-premium-dark">
                <div class="widget-content-outer">
                   <div class="widget-content-wrapper">
                      <div class="widget-content-left">
