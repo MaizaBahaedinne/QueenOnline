c4a0426b06310765d43d527c195cd541ae374fee
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-09-21 18:18:07 +0100

    Update Reservation_model.php

--- application/models/Reservation_model.php
+++ application/models/Reservation_model.php
@@ -222,7 +222,7 @@ class Reservation_model extends CI_Model
         $this->db->join('tbl_users as Locataire', 'Locataire.userId = BaseTbl.locataireId','left');
         
         $this->db->where('BaseTbl.statut IN (0,1) ');
-        $this->db->where('Year(BaseTbl.dateFin) >= 2023 ');
+        
         $this->db->group_by('BaseTbl.locataireId');
         $this->db->order_by('count(userId) DESC');
 
