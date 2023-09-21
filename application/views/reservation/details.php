bbb6832dcdb770ea22c3fdcb20185a78ee69d3c4
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-09-21 23:48:10 +0100

    Update details.php

--- application/views/reservation/details.php
+++ application/views/reservation/details.php
@@ -46,12 +46,12 @@
                                           html:
                                             '1. Vérification de la date de paiement <br>' +
                                             '2. Invitation à se présenter aux bureaux <a href="tel:<?php echo $clientInfo->mobile; ?>"><?php echo $clientInfo->mobile; ?></a> <br> ' +
-                                            '3. Réunion de résiliation de contrat de location<br> <a href:"">Le client est pret à signer</a>',
+                                            '3. Réunion de résiliation de contrat de location<br> <br><b>Le client est pret à signer</b>',
                                           showCloseButton: true,
                                           showCancelButton: true,
                                           focusConfirm: false,
                                           confirmButtonText:
-                                            'Go',
+                                            '<a href="<?php echo base_url() ?>">Go</a>',
                                           confirmButtonAriaLabel: 'Pas encore',
                                           cancelButtonText:
                                             '<i class="fa fa-thumbs-down"></i>',
