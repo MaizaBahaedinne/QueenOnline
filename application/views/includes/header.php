6b13011350b0da0a72b9a1e4ba4172f75e7768b4
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-09-21 22:52:44 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1275,108 +1275,43 @@
                         </ul>
                         <ul class="vertical-nav-menu">
 
+                            <li class="app-sidebar__heading">Comptabilité</li>
+
                             <li class="">
                                 <a href="#" aria-expanded="false">
-                                <i class="metismenu-icon pe-7s-diamond"></i> Elements
+                                <i class="metismenu-icon pe-7s-diamond"></i> Revenue
                                 <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
                                 </a>
                                 <ul class="mm-collapse" style="height: 7.04px;">
                                 <li>
-                                <a href="#">
-                                <i class="metismenu-icon"></i> Buttons
-                                <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
-                                </a>
-                                <ul class="mm-collapse">
-                                <li>
-                                <a href="elements-buttons-standard.html">
-                                <i class="metismenu-icon"></i>Standard
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-buttons-pills.html">
-                                <i class="metismenu-icon"></i>Pills
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-buttons-square.html">
-                                <i class="metismenu-icon"></i>Square
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-buttons-shadow.html">
-                                <i class="metismenu-icon"></i>Shadow
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-buttons-icons.html">
-                                <i class="metismenu-icon"></i>With Icons
-                                </a>
-                                </li>
-                                </ul>
-                                </li>
-                                <li>
-                                <a href="elements-dropdowns.html">
-                                <i class="metismenu-icon"></i>Dropdowns
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-icons.html">
-                                <i class="metismenu-icon"></i>Icons
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-badges-labels.html">
-                                <i class="metismenu-icon"></i>Badges
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-cards.html">
-                                <i class="metismenu-icon"></i>Cards
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-loaders.html">
-                                <i class="metismenu-icon"></i>Loading Indicators
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-list-group.html">
-                                <i class="metismenu-icon"></i>List Groups
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-navigation.html">
-                                <i class="metismenu-icon"></i>Navigation Menus
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-timelines.html">
-                                <i class="metismenu-icon"></i>Timeline
-                                </a>
-                                </li>
-                                <li>
-                                <a href="elements-utilities.html">
-                                <i class="metismenu-icon"></i>Utilities
-                                </a>
-                                </li>
-                                </ul>
-                                </li>
-                            
-                            <li class="app-sidebar__heading">Comptabilité</li>
-                            <?php if($role == 1 || $role == 2 ){  ?>
-                            <li>
                                 <a href="<?php echo base_url() ?>Finance">
                                     <i class="metismenu-icon pe-7s-piggy"></i>Revenue /Salle
                                 </a>
-                            </li>
-                             <?php }  ?>
-                             <?php if($role == 1 || $role == 2 ){  ?>
-                            <li>
+                                </li>
+                                <li>
                                 <a href="<?php echo base_url() ?>Finance/photographe">
                                     <i class="metismenu-icon pe-7s-piggy"></i>Revenue /Photographe
                                 </a>
+                                </li>
+                                
+                                
+                                <li>
+                                <a href="<?php echo base_url() ?>Finance/troupe">
+                                    <i class="metismenu-icon pe-7s-piggy"></i>Revenue /Troupe
+                                </a>
+                                </li>
+                               
+                                <li>
+                                 <a href="<?php echo base_url() ?>Finance/troupe">
+                                    <i class="metismenu-icon pe-7s-piggy"></i>Revenue /Troupe
+                                </a>
+                                </li>
+                                </ul>
                             </li>
-                             <?php }  ?>
+                            
+
+                            
+                             
                              <?php if($role == 1 || $role == 2 ){  ?>
                             <li>
                                 <a href="<?php echo base_url() ?>Finance/troupe">
