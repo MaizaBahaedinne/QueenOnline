5c81ea617c69f4af0ab13dfe27025ed5bd78907b
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-09-25 14:42:34 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -348,7 +348,7 @@ Increased by
             
         
       </div>
-      <div class="col-md-12">
+      <div class="row">
          
          <div class="col-lg-4">
             <div class="mb-3 card">
