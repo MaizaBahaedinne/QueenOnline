2de289089fbfcf2a6b0cde7d0d7435a32cde04fc
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2023-09-26 12:58:54 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -147,9 +147,7 @@
                 }, {
                   name: 'en attente',
                   data: [ <?php 
-                      foreach ($variable as $key => $value) {
-                         // code...
-                       } ( $i = 2018 ; $i <= date('Y')+1 ; $i++) {
+                      for ( $i = 2018 ; $i <= date('Y')+1 ; $i++) {
                           foreach($reservationEnAttent as $data) {
                               if($data->yearDate == $i ){
                                     echo $data->countRes.',';
@@ -160,7 +158,21 @@
                         } 
                      ?>
                   ]
-                }, ],
+                }, {
+                  name: 'Annulée',
+                  data: [ <?php 
+                    for ( $i = 2018 ; $i <= date('Y')+1 ; $i++) {
+                          foreach($reservationAnnule as $data) {
+                              if($data->yearDate == $i ){
+                                    echo $data->countRes.',';
+                              } else {
+                                    echo "0,";
+                              }
+                          }
+                        } 
+                    ?>
+                  ]
+                }],
                 chart: {
                   height: 580,
                   type: 'bar',
