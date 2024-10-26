582f6c2672a308fb88fcb63562913a177f600b36
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2024-10-26 22:19:18 +0200

    Update SMS.php

--- application/controllers/SMS.php
+++ application/controllers/SMS.php
@@ -33,13 +33,13 @@ class SMS extends BaseController
             
                     foreach ($data["smsRecords"]  as $record) {
         
-                       //  $this->sendSMS($record->destination , $record->text );  
+                         $this->sendSMS($record->destination , $record->text );  
                          $smsInfo = array(
                               'sendDate'=>date('Y-m-d H:i:s') ,
                               'statut'=>0 ,
 
                             );
-                        // $this->sms_model->editSms($smsInfo,$record->smsId) ;
+                        $this->sms_model->editSms($smsInfo,$record->smsId) ;
                        }   
 
 
