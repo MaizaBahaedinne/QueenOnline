0e7934e2c406038c60eacdaad59fd44899f529f3
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2024-10-26 23:37:07 +0200

    Update SMS.php

--- application/controllers/SMS.php
+++ application/controllers/SMS.php
@@ -43,57 +43,58 @@ class SMS extends BaseController
     }
 
 
-    function http_response($url)
-        {
-            $ch = curl_init();
-
-            $options = array(
-                CURLOPT_URL            => $url ,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_HEADER         => false,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_ENCODING       => "",
-                CURLOPT_AUTOREFERER    => true,
-                CURLOPT_CONNECTTIMEOUT => 120,
-                CURLOPT_TIMEOUT        => 120,
-                CURLOPT_MAXREDIRS      => 10,
-                CURLOPT_SSL_VERIFYPEER => false,
-            );
-            curl_setopt_array( $ch, $options );
-            $response = curl_exec($ch);
-           
-            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-            if ( $httpCode != 200 ){
-                return "Return code is {$httpCode} \n"
-                    .curl_error($ch);
-            } else {
-                //echo "<pre>".htmlspecialchars($response)."</pre>";
-                return $response;
-            }
-
-            curl_close($ch);
-        }
-
-
-
+             
         function sendSMS($myMobile, $mySms)
-        {
+        { 
+            $mySender = 'Queen park';
+            $key = "ns1PwxEKAljejzi3RSBAHPsoQl/P9s0jtrXDkRb4j6sjNpzNER8aprZNyzyAuLlteKM222LwbgBRrlBCvFDV4YlQbSvBZMYA/Ye3r0ggsYQ=";
 
-        $mySender = 'Queen park';
-        $key = "ns1PwxEKAljejzi3RSBAHPsoQl/P9s0jtrXDkRb4j6sjNpzNER8aprZNyzyAuLlteKM222LwbgBRrlBCvFDV4YlQbSvBZMYA/Ye3r0ggsYQ=";
+            $myDate = '26/10/2024';
+            $myTime = '21:33';
 
-        $Url_str ="www.tunisiesms.tn/client/Api/Api.aspx?fct=sms&key=%KEY%&mobile=%MSISDN%&sms=%SMS%&sender=%SENDER%";
-
-        $Url_str = str_replace("%MSISDN%",$myMobile,$Url_str);
-        $Url_str = str_replace("%SMS%",urlencode($mySms),$Url_str);
-        $Url_str = str_replace("%SENDER%",urlencode($mySender),$Url_str);
-        $Url_str = str_replace("%KEY%",urlencode($key),$Url_str);
-
-        
-        echo $this->http_response($Url_str);
-
+            $Url_str ="https://app.tunisiesms.tn/Api/Api.aspx?fct=sms&key=MYKEY&mobile=XXXXXXXX&sms=Hello+World&sender=YYYYYYY&date=jj/mm/aaaa&heure=hh:mm:ss";
+                                            
+            $Url_str = str_replace("XXXXXXXX",$myMobile,$Url_str);
+            $Url_str = str_replace("Hello+World",$mySms,$Url_str);
+            $Url_str = str_replace("YYYYYYY",$mySender,$Url_str);
+            $Url_str = str_replace("jj/mm/aaaa",myDate,$Url_str);
+            $Url_str = str_replace("hh:mm:ss",myTime,$Url_str);
+            $Url_str = str_replace("MYKEY",urlencode($key),$Url_str);
+                                            
+            echo http_response($Url_str);
         }
+
+        function http_response($url)
+        { 
+           $ch = curl_init(); 
+           $options = array( 
+           CURLOPT_URL            =--> $url , 
+           CURLOPT_RETURNTRANSFER => true, 
+           CURLOPT_HEADER         => false, 
+           CURLOPT_FOLLOWLOCATION => true, 
+           CURLOPT_ENCODING       => '', 
+           CURLOPT_AUTOREFERER    => true, 
+           CURLOPT_CONNECTTIMEOUT => 120, 
+           CURLOPT_TIMEOUT        => 120,  
+           CURLOPT_MAXREDIRS      => 10,  
+           CURLOPT_SSL_VERIFYPEER => false, 
+           ); 
+           curl_setopt_array( $ch, $options );  
+           $response = curl_exec($ch); 
+           $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+           if ( $httpCode != 200 ) 
+           { 
+           return 'Return code is {$httpCode}' 
+           .curl_error($ch); 
+           } 
+            else  
+           { 
+           return $response; 
+            } 
+           curl_close($ch);
+        }
+            
+        
 
 
    
