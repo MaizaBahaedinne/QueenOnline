7d2c1eed45a5e3c6e78ea431065abfbe271eb6cd
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2024-10-27 00:39:18 +0200

    Update SMS.php

--- application/controllers/SMS.php
+++ application/controllers/SMS.php
@@ -51,7 +51,7 @@ class SMS extends BaseController
                 $mySender = 'Queen park';
                 $key = "s1PwxEKAljejzi3RSBAHPsoQl/P9s0jtrXDkRb4j6sjNpzNER8aprZNyzyAuLlteKM222LwbgBRrlBCvFDV4YlQbSvBZMYA/Ye3r0ggsYQ=";
 
-                $Url_str ="https://app.tunisiesms.tn/Api/Api.aspx?fct=sms&key=%KEY%&mobile=%MSISDN%&sms=%SMS%&sender=%SENDER%";
+                $Url_str ="https://app.tunisiesms.tn/Api/Api.aspx?fct=sms&key=s1PwxEKAljejzi3RSBAHPsoQl/P9s0jtrXDkRb4j6sjNpzNER8aprZNyzyAuLlteKM222LwbgBRrlBCvFDV4YlQbSvBZMYA/Ye3r0ggsYQ=&mobile=%MSISDN%&sms=%SMS%&sender=%SENDER%";
 
                 $Url_str = str_replace("%MSISDN%",$myMobile,$Url_str);
                 $Url_str = str_replace("%SMS%",urlencode($mySms),$Url_str);
@@ -65,36 +65,34 @@ class SMS extends BaseController
 
 
                 function http_response($url)
-                {
-                    $ch = curl_init();
-
-                    $options = array(
-                        CURLOPT_URL            => $url ,
-                        CURLOPT_RETURNTRANSFER => true,
-                        CURLOPT_HEADER         => false,
-                        CURLOPT_FOLLOWLOCATION => true,
-                        CURLOPT_ENCODING       => "",
-                        CURLOPT_AUTOREFERER    => true,
-                        CURLOPT_CONNECTTIMEOUT => 120,
-                        CURLOPT_TIMEOUT        => 120,
-                        CURLOPT_MAXREDIRS      => 10,
-                        CURLOPT_SSL_VERIFYPEER => false,
-                    );
-                    curl_setopt_array( $ch, $options );
-                    $response = curl_exec($ch);
-                   
-                    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-                    if ( $httpCode != 200 ){
-                        return "Return code is {$httpCode} \n"
-                            .curl_error($ch);
-                    } else {
-                        //echo "<pre>".htmlspecialchars($response)."</pre>";
-                        return $response;
+                    { 
+                       $ch = curl_init(); 
+                       $options = array( 
+                       CURLOPT_URL            =--> $url , 
+                       CURLOPT_RETURNTRANSFER => true, 
+                       CURLOPT_HEADER         => false, 
+                       CURLOPT_FOLLOWLOCATION => true, 
+                       CURLOPT_ENCODING       => '', 
+                       CURLOPT_AUTOREFERER    => true, 
+                       CURLOPT_CONNECTTIMEOUT => 120, 
+                       CURLOPT_TIMEOUT        => 120,  
+                       CURLOPT_MAXREDIRS      => 10,  
+                       CURLOPT_SSL_VERIFYPEER => false, 
+                       ); 
+                       curl_setopt_array( $ch, $options );  
+                       $response = curl_exec($ch); 
+                       $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+                       if ( $httpCode != 200 ) 
+                       { 
+                       return 'Return code is {$httpCode}' 
+                       .curl_error($ch); 
+                       } 
+                        else  
+                       { 
+                       return $response; 
+                        } 
+                       curl_close($ch);
                     }
-
-                    curl_close($ch);
-                }
 
     
 
