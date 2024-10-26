f067192b57f68bf3fef21f92fb6fa9bd7477cea6
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2024-10-27 00:55:36 +0200

    Update SMS.php

--- application/controllers/SMS.php
+++ application/controllers/SMS.php
@@ -57,11 +57,8 @@ class SMS extends BaseController
                 $Url_str = str_replace("%MSISDN%",$myMobile,$Url_str);
                 $Url_str = str_replace("%SMS%",urlencode($mySms),$Url_str);
                 $Url_str = str_replace("%SENDER%",urlencode($mySender),$Url_str);
-              
-
 
                 echo $this->http_response($Url_str);
-
                 }
 
 
