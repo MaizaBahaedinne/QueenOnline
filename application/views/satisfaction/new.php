3e1070f105c54be3e2ce2dce1695351e05d9b100
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-02-27 21:57:10 +0100

    Update new.php

--- application/views/satisfaction/new.php
+++ application/views/satisfaction/new.php
@@ -51,9 +51,8 @@
                         <div class="col-lg-12 col-md-12">
                             <div class="main-card mb-3 card">
                                 <div class="card-body">
-                                    <div id="smartwizard">
-                                        <div class="form-wizard-content">
-                                            <div id="step-1">
+                                  
+                                        
                                                 <div class="form-row">
                                                     <div class="col-sm-6">
                                                         <div class="form-group">
@@ -146,27 +145,10 @@
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                        <h4>Confirmation</h4>
-                                        <div class="no-results">
-                                            <div class="swal2-icon swal2-success swal2-animate-success-icon">
-                                                <div class="swal2-success-circular-line-left" style="background-color: rgb(255, 255, 255);"></div>
-                                                <span class="swal2-success-line-tip"></span>
-                                                <span class="swal2-success-line-long"></span>
-                                                <div class="swal2-success-ring"></div>
-                                                <div class="swal2-success-fix" style="background-color: rgb(255, 255, 255);"></div>
-                                                <div class="swal2-success-circular-line-right" style="background-color: rgb(255, 255, 255);"></div>
-                                            </div>
-                                            <div class="results-subtitle mt-4">Fini!</div>
-                                            <div class="results-title">Vous êtes arrivé à la dernière étape de l'assistant de formulaire</div>
-                                            <div class="mt-3 mb-3"></div>
-                                            <div class="text-center">
-                                                <button type="submit" onclick="LoadingDiv()" class="btn-shadow btn-wide btn btn-success btn-lg" />
-                                                    Confirmer<span class="spinner"></span>
-                                            </div>
-                                        </div>
-                                    </div>
+                                           
+                                        
+                                        
+                                   
                                 </div>
                             </div>
                             <div class="clearfix">
