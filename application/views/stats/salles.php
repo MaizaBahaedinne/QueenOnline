58bb6989ebbb8aa67141beb29d2f3d0aa6829863
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-02-27 20:21:30 +0100

    Update salles.php

--- application/views/stats/salles.php
+++ application/views/stats/salles.php
@@ -16,11 +16,9 @@
    </div>
    <div class="tabs-animation">
       <div class="row">
-
-		<?php for ($i=2018 ; $i <= date('Y') ; $i++) { ?>
-
-		<div class="col-md-4 col-xl-4">
-			<div class="mb-3 card">
+         <?php for ($i=2018 ; $i <= date('Y') ; $i++) { ?>
+         <div class="col-md-4 col-xl-4">
+            <div class="mb-3 card">
                <div class="card-header-tab card-header">
                   <div class="card-header-title">
                      <i class="header-icon lnr-rocket icon-gradient bg-tempting-azure"> </i>
@@ -35,38 +33,50 @@
                   </div>
                </div>
                <div class="widget-chart p-0">
-                  <div id="Salle<?php echo $i; ?>"></div>
+                  <canvas id="Salle<?php echo $i; ?>" style="width: 100%; height: 500px;"></canvas>
                </div>
                <div class="divider mb-0"></div>
             </div>
-			<script type="text/javascript">
-               var options = {
-                     series: [<?php foreach($stats as $data) {  if ($i == $data->da ) { ; echo $data->COUNTRES.',' ; }  } ?>],
-                     chart: {
-                 
-                     type: 'pie',
-                   },
-                   labels: [<?php foreach($stats as $data) {   if ($i == $data->da ){ echo '"'.$data->sallename.'",' ; } }   ?>],
-                   responsive: [{
-                     breakpoint: 500,
-                     options: {
-                       chart: {
-                         width: 100
+            <script type="text/javascript">
+               document.addEventListener("DOMContentLoaded", function () {
+                   var ctx = document.getElementById('Salle<?php echo $i; ?>').getContext('2d');
+               
+                   // Données PHP converties en JavaScript
+                   var labels = [<?php foreach($stats as $data) { if ($i == $data->da) { echo '"'.$data->sallename.'",'; } } ?>];
+                   var dataValues = [<?php foreach($stats as $data) { if ($i == $data->da) { echo $data->COUNTRES.','; } } ?>];
+               
+                   var chart = new Chart(ctx, {
+                       type: 'pie',
+                       data: {
+                           labels: labels,
+                           datasets: [{
+                               data: dataValues,
+                               backgroundColor: [
+                                   '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'
+                               ],
+                               borderColor: '#ffffff',
+                               borderWidth: 1
+                           }]
                        },
-                       legend: {
-                         position: 'buttom'
+                       options: {
+                           responsive: true,
+                           maintainAspectRatio: false,
+                           plugins: {
+                               title: {
+                                   display: true,
+                                   text: 'Répartition des Salles',
+                                   font: { size: 18 }
+                               },
+                               legend: {
+                                   position: 'bottom'
+                               }
+                           }
                        }
-                     }
-                   }]
-                   };
-               
-                   var chart = new ApexCharts(document.querySelector("#Salle<?php echo $i; ?>"), options);
-                   chart.render();
-               
+                   });
+               });
             </script>
-             </div>
-            <?php } ?>
-
-		</div>
-	</div>
+         </div>
+         <?php } ?>
+      </div>
+   </div>
 </div>
