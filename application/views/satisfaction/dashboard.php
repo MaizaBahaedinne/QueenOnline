1e10f23101b9ed11aa27776f042fa56f6169145d
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-03-22 17:47:02 +0100

    Update dashboard.php

--- application/views/satisfaction/dashboard.php
+++ application/views/satisfaction/dashboard.php
@@ -17,14 +17,15 @@
    <div class="tabs-animation">
       <div class="row">
          <?php for ($i=1; $i<=12 ; $i+1) { 
-           echo "<h4>".$i."</h4>"?>
-         <?php foreach ( $satisfactionSalles as $satisfactionSalle ) { if ($satisfactionSalle->MONTH == $i ){  ?>
+            echo "<h4>".$i."</h4>"?>
+         <?php foreach ( $satisfactionSalles as $satisfactionSalle ) { 
+            if ($satisfactionSalle->MONTH == $i ) {  ?>
          <div class="col-md-4 col-xl-4">
             <div class="mb-3 card">
                <div class="card-header-tab card-header">
                   <div class="card-header-title">
                      <i class="header-icon lnr-rocket icon-gradient bg-tempting-azure"> </i>
-                       <?php echo $satisfactionSalle->salle ; ?>
+                     <?php echo $satisfactionSalle->salle ; ?>
                   </div>
                   <div class="btn-actions-pane-right text-capitalize">
                      <a class="btn-wide btn-outline-2x btn btn-outline-primary btn-sm" href="<?php echo base_url() ?>Reservation" >Voir tout</a>
@@ -36,55 +37,61 @@
                </div>
                <div class="widget-chart p-0">
                   <table width="100%">
-                      <tr>
-                          <td width="20%">Salle</td>
-                          <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_salle)) ?></td>
-                          <td><?php echo round($satisfactionSalle->avg_salle, 1) ?></td> <!-- Rounded to 1 decimal place -->
-                      </tr>
-                      <tr>
-                          <td>Service</td>
-                          <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_service)) ?></td>
-                          <td><?php echo round($satisfactionSalle->avg_service, 1) ?></td> <!-- Rounded to 1 decimal place -->
-                      </tr>
-                      <tr>
-                          <td>Propreté</td>
-                          <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_proprete)) ?></td>
-                          <td><?php echo round($satisfactionSalle->avg_proprete, 1) ?></td> <!-- Rounded to 1 decimal place -->
-                      </tr>
-                      <tr>
-                          <td>Lumière</td>
-                          <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_lumiere)) ?></td>
-                          <td><?php echo round($satisfactionSalle->avg_lumiere, 1) ?></td> <!-- Rounded to 1 decimal place -->
-                      </tr>
-                      <tr>
-                          <td>Décoration</td>
-                          <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_decoration)) ?></td>
-                          <td><?php echo round($satisfactionSalle->avg_decoration, 1) ?></td> <!-- Rounded to 1 decimal place -->
-                      </tr>
-                      <tr>
-                          <td>Photographe</td>
-                          <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_photographe)) ?></td>
-                          <td><?php echo round($satisfactionSalle->avg_photographe, 1) ?></td> <!-- Rounded to 1 decimal place -->
-                      </tr>
-                      <tr>
-                          <td>Troupe</td>
-                          <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_musicale)) ?></td>
-                          <td><?php echo round($satisfactionSalle->avg_musicale, 1) ?></td> <!-- Rounded to 1 decimal place -->
-                      </tr>
-                      <tr>
-                          <td>Voiture</td>
-                          <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_voiture)) ?></td>
-                          <td><?php echo round($satisfactionSalle->avg_voiture, 1) ?></td> <!-- Rounded to 1 decimal place -->
-                      </tr>
+                     <tr>
+                        <td width="20%">Salle</td>
+                        <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_salle)) ?></td>
+                        <td><?php echo round($satisfactionSalle->avg_salle, 1) ?></td>
+                        <!-- Rounded to 1 decimal place -->
+                     </tr>
+                     <tr>
+                        <td>Service</td>
+                        <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_service)) ?></td>
+                        <td><?php echo round($satisfactionSalle->avg_service, 1) ?></td>
+                        <!-- Rounded to 1 decimal place -->
+                     </tr>
+                     <tr>
+                        <td>Propreté</td>
+                        <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_proprete)) ?></td>
+                        <td><?php echo round($satisfactionSalle->avg_proprete, 1) ?></td>
+                        <!-- Rounded to 1 decimal place -->
+                     </tr>
+                     <tr>
+                        <td>Lumière</td>
+                        <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_lumiere)) ?></td>
+                        <td><?php echo round($satisfactionSalle->avg_lumiere, 1) ?></td>
+                        <!-- Rounded to 1 decimal place -->
+                     </tr>
+                     <tr>
+                        <td>Décoration</td>
+                        <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_decoration)) ?></td>
+                        <td><?php echo round($satisfactionSalle->avg_decoration, 1) ?></td>
+                        <!-- Rounded to 1 decimal place -->
+                     </tr>
+                     <tr>
+                        <td>Photographe</td>
+                        <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_photographe)) ?></td>
+                        <td><?php echo round($satisfactionSalle->avg_photographe, 1) ?></td>
+                        <!-- Rounded to 1 decimal place -->
+                     </tr>
+                     <tr>
+                        <td>Troupe</td>
+                        <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_musicale)) ?></td>
+                        <td><?php echo round($satisfactionSalle->avg_musicale, 1) ?></td>
+                        <!-- Rounded to 1 decimal place -->
+                     </tr>
+                     <tr>
+                        <td>Voiture</td>
+                        <td><?php echo str_repeat("⭐", round($satisfactionSalle->avg_voiture)) ?></td>
+                        <td><?php echo round($satisfactionSalle->avg_voiture, 1) ?></td>
+                        <!-- Rounded to 1 decimal place -->
+                     </tr>
                   </table>
-
                </div>
                <div class="divider mb-0"></div>
             </div>
-            
          </div>
          <?php } } 
-         echo "</hr>" ;} ?>
+            echo "</hr>" ;} ?>
       </div>
    </div>
 </div>
