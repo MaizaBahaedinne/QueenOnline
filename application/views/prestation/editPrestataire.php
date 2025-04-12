747ecd929031210967439e9ac408943972f89db7
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-04-12 23:04:00 +0100

    Create editPrestataire.php

---
+++ application/views/prestation/editPrestataire.php
@@ -0,0 +1,33 @@
+
+<div class="app-main__inner">
+  <div class="app-page-title">
+    <div class="page-title-wrapper">
+      <div class="page-title-heading">
+        <div class="page-title-icon">
+          <i class="pe-7s-car icon-gradient bg-tempting-azure"></i>
+        </div>
+        <div>
+          Prestataire
+          <div class="page-title-subheading">Nouveau prestataire</div>
+        </div>
+      </div>
+      <div class="page-title-actions">
+       
+        <div class="d-inline-block">
+         
+          
+        </div>
+      </div>
+    </div>
+  </div>
+    <form action="<?php echo base_url(); ?>Prestation/editPrestataire" method="post">                
+             <input class="form-control" type="text" name="nom" value="<?php echo $pack->nom  ?>">   
+             <input class="form-control" type="number" min="50" placeholder="TND"  name="prix" value="<?php echo $pack->prix  ?>" >
+             <textarea class="form-control" row="20" name="description">value="<?php echo $pack->description  ?>"</textarea>
+             <select  class="form-control"  name="type">
+                <option value="Chanteur" >Chanteur</option>
+                <option value="Notaire" >Notaire</option>
+                <option value="prestataire" >prestataire</option>
+             </select>
+    </form>
+</div>
