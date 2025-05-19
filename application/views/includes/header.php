adfae5e80400546fec1c1fa4c11fe214a1e07666
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-05-19 16:05:30 +0200

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -475,7 +475,7 @@
                                     <div class="btn-group">
                                        
                                         <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="p-0 btn">
-                                             <img width="42" class="rounded-circle" src="https://www.queenpark.tn/assets/img/teams/<?php echo $avatar ?>" alt="">
+                                             <img width="42" class="rounded-circle" src="<?php echo $avatar ?>" alt="">
                                             <i class="fa fa-angle-down ml-2 opacity-8"></i>
                                         </a>
                                         
