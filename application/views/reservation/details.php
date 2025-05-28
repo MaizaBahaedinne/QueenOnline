473c79b8ce2b2daacc665775370eb61efbaae3cf
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2025-05-28 13:49:22 +0200

    Update details.php

--- application/views/reservation/details.php
+++ application/views/reservation/details.php
@@ -564,7 +564,7 @@
                                                         
                                             
                                         </td>
-                                        <td>Avance <hr>Reste</td>
+                                        <td>Avance <hr>Reste<br><p style="color:white;">Paiement dû avant le : 01/05/2025</p></td>
                                         <td>
                                             <b style="color: green"><?php echo $contratInfo->avance; ?> DT</b>
                                             <hr>
